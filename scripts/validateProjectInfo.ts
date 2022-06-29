46cedf870a2654e10b0501a5d5dea2088f33481d
Veronica <veronica.21.coutts@gmail.com>
2022-06-29 21:17:43 +0200

    Deployer address check (WIP)

--- scripts/validateProjectInfo.ts
+++ scripts/validateProjectInfo.ts
@@ -1,13 +1,14 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
 import { providers, Contract } from "ethers";
 import tokenInterface from "../src/tokenInterface.json";
 import opTokenList from "../optimism.tokenlist.json";
 // TODO this needs to go
-import newTokenFile from '../projects/standard-bridge/example.json';
+import newTokenFile from "../projects/standard-bridge/example.json";
+import fetch from "node-fetch";
 
-var args = process.argv.slice(2);
+// var args = process.argv.slice(2);
 
 // FUTURE add flag to validate a single project passed in or all projects
 
@@ -17,11 +18,6 @@ export const network = {
   KOVAN: 42,
   OP_KOVAN: 69
 };
-
-const networkMainnet = 1;
-const networkOptimism = 10;
-const networkKovan = 42;
-const networkOpKovan = 69;
 
 export const oppositeChainIdMap = {
   [network.MAINNET]: network.OP_MAINNET,
@@ -39,6 +35,8 @@ export const chainIdLayerMap = {
 
 const infuraKey = process.env.INFURA_KEY || "84842078b09946638c03157f83405213";
 
+const factoryDeployer:string = "0x4200000000000000000000000000000000000012";
+
 const networkURLMap = {
   1: `https://mainnet.infura.io/v3/${infuraKey}`,
   10: `https://mainnet.optimism.io`,
@@ -53,187 +51,218 @@ const networkMap = {
   69: "Optimistic Kovan"
 };
 
-let projectTokens = {
-  mainnet: [],
-  optimism: [],
-  kovan: [],
-  opKovan: []
+type TokenListing = {
+  chainId: number;
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  logoURI: string;
+  factoryDeployed: Boolean;
+  extensions?: string;
 };
 
 async function main() {
-  console.log(args)
+  // console.log(args)
 
   // TODO get file in here
+  // let filePath = './projects/standard-bridge/' + args[1];
+  // console.log(filePath)
+  // let resolvedPath = path.resolve(filePath);
+  // readFile(resolvedPath);
+  // const recipientsObject = JSON.parse(fs.readFileSync(options.input, { encoding: "utf8" }));
+  // Getting their supplied token data
   let newToken = newTokenFile;
-  
-  let configuration = [];
-  
+
+  // TODO verify off chain data
+
   // Getting onchain info
   for (const token of newToken.tokens) {
-    let tokenInfo = {
-      chainId: token.chainId,
-      address: token.address,
-      name: "",
-      symbol: "",
-      decimals: "",
-      logoURI: newToken.logoURI,
-      extensions: ""
-    };
+    // let onchainListing:TokenListing =
+    await getOnchainInfo(token.chainId, token.address, newToken.logoURI);
 
-    const result = opTokenList.tokens.find( ({ address }) => address === token.address );
-    if(result != undefined) {
-      throw Error("Token address already listed! Please use update not add.");
-    }
+    // console.log(token)
+    // console.log(onchainListing)
 
-    console.log("Fetch: Info for token " + token.address + " on chain ID " + token.chainId);
-
-    configuration.push(tokenInfo.chainId);
-
-    const currentNetwork = networkURLMap[tokenInfo.chainId];
-    const currentChainProvider = new providers.JsonRpcProvider(currentNetwork);
-
-    const contract = new Contract(
-      tokenInfo.address,
-      tokenInterface,
-      currentChainProvider
-    );
-
-    let onchainName = "";
-    let onchainSymbol = "";
-    let onchainDecimals = "";
-
-    try {
-      onchainName = await contract.name();
-      onchainSymbol = await contract.symbol();
-      onchainDecimals = await contract.decimals();
-
-      tokenInfo.name = onchainName;
-      tokenInfo.symbol = onchainSymbol;
-      tokenInfo.decimals = onchainDecimals;
-    } catch (error) {
-      throw Error("Unable to fetch token name, symbol or decimal. " + 
-      "\nPotential Fix: Ensure your token contract conforms to the ERC20 standard. " + 
-      "Check your network and token addresses match. " + 
-      "For non-standard tokens please make a custom token request.");
-    }
-
-    // TODO check token complies with ERC20 
-
-    switch (tokenInfo.chainId) {
-      case 1:
-        projectTokens.mainnet.push(tokenInfo);
-        break;
-      case 10:
-        projectTokens.optimism.push(tokenInfo);
-        break;
-      case 42: 
-        projectTokens.kovan.push(tokenInfo);
-        break;
-      case 69: 
-        projectTokens.opKovan.push(tokenInfo);
-        break;
-      default:
-        throw Error("Unsupported network provided");
-        break;
-    }
-    console.log("Success: Pulled token information.");
-  };
-
-  console.log(projectTokens);
-
-  const validConfigurations = {
-    testing: [42, 69],
-    full: [1, 10, 42, 69],
-    unbridgeable: [10, 69]
-  };
+    // const result = opTokenList.tokens.find( ({ address }) => address === token.address );
+    // if(result != undefined) {
+    //   // TODO check if info is the same in case script has been run before
+    //   throw Error("Token address already listed! Please use update not add.");
+    // }
+  }
 
   // Checking config is valid
-  if(configuration.length == 2) {
-    if(configuration[0] == 42 && configuration[1] == 69) {
-      console.log("Success: Token configuration valid. Configuration: Testing");
-    } else if(configuration[0] == 10 && configuration[1] == 69) {
-      console.log("Success: Token configuration valid. Configuration: Unbridgeable");
-    } else {
-      throw Error("Invalid config.\nPotential Fix: Two token configurations can either be " +
-      "unbridgeable or testing. For more information see the standard bridge README");
-    }
-  } else if(
-    configuration.length == 4 &&
-    configuration[0] == 1 &&
-    configuration[0] == 10 &&
-    configuration[0] == 42 &&
-    configuration[0] == 96
+  // A valid config is defined in the README as one of three:
+  // - Testing (Kovan & Optimism Kovan)
+  // - Unbridgeable (Optimism & Optimism Kovan)
+  // - Full suite (Ethereum mainnet, Optimism, Kovan & Optimism Kovan)
+  // Any other configuration will result in a detailed error message.
+  // checkConfig([projectTokenKovan, projectTokenOpKovan]);
+
+  // Writes verified data to file.
+  // writeFile("./projects/standard-bridge/example.json", newToken);
+}
+
+async function getOnchainInfo(
+  chainId: number,
+  tokenAddress: string,
+  logoURI: string
+): Promise<TokenListing> {
+  let onchainListing: TokenListing = {
+    chainId: chainId,
+    address: tokenAddress,
+    name: "",
+    symbol: "",
+    decimals: 0,
+    logoURI: logoURI,
+    factoryDeployed: false
+  };
+
+  if (
+    chainId != network.MAINNET &&
+    chainId != network.OP_MAINNET &&
+    chainId != network.KOVAN &&
+    chainId != network.OP_KOVAN
   ) {
-
-  } else {
-    throw Error("Invalid config.\nPotential Fix: Please see acceptable configs in the standard bridge README");
+    throw Error("Chain ID provided is invalid.");
   }
-  
-  // writeFile(newToken, newToken); 
-}
 
-function readFile(path: any) {
+  console.log(
+    "Fetch: Info for token " + tokenAddress + " on " + networkMap[chainId]
+  );
+
+  const currentNetwork = networkURLMap[chainId];
+  const currentChainProvider = new providers.JsonRpcProvider(currentNetwork);
+
+  const contract = new Contract(
+    tokenAddress,
+    tokenInterface,
+    currentChainProvider
+  );
+
   try {
-    fs.readFileSync(path, {encoding:'utf8', flag:'r'});
-  } catch (err) {
-    console.log(err);
-    return 'Something went wrong reading the file.';
+    onchainListing.name = await contract.callStatic.name();
+    onchainListing.symbol = await contract.symbol();
+    onchainListing.decimals = Number(await contract.decimals());
+  } catch (error) {
+    console.log(error);
+    throw Error(
+      "Unable to fetch token name, symbol or decimal. " +
+        "\nPotential Fix: Ensure your token contract conforms to the ERC20 standard. " +
+        "Check the provided token exists on the given network. " +
+        "For non-standard tokens please make a custom token request."
+    );
   }
-}
+  console.log("Success: Pulled token information.");
 
-function writeFile(path: any, data: any) {
-  try {
-      let stringData = JSON.stringify(data)
-      fs.writeFileSync(path, stringData, {
-      flag: 'w',
-    });
-  } catch (err) {
-    console.log(err);
-    return 'Something went wrong';
+  let result = await getContractDeployer(
+    onchainListing.address,
+    onchainListing.chainId
+  );
+  console.log(result);
+
+  if(result == factoryDeployer) {
+    
   }
+
+  console.log(onchainListing);
+  return onchainListing;
 }
 
 function checkConfig(tokens: Array<TokenListing>) {
-  if(tokens.length > 4) {
-    throw Error("Max tokens provided is greater than 4.\n" + 
-      "Potential Fix: There are only 4 valid chains for token listing. " + 
-      "Ethereum mainnet, Optimism mainnet, Kovan testnet and Optimism Kovan testnet. " +
-      "Please check your provided addresses and make the necessary corrections."
+  if (tokens.length > 4) {
+    throw Error(
+      "Max tokens provided is greater than 4.\n" +
+        "Potential Fix: There are only 4 valid chains for token listing. " +
+        "Ethereum mainnet, Optimism mainnet, Kovan testnet and Optimism Kovan testnet. " +
+        "Please check your provided addresses and make the necessary corrections."
     );
-  } else if(tokens.length == 4) {
-    if(
+  } else if (tokens.length == 4) {
+    if (
       tokens[0].chainId == network.MAINNET &&
       tokens[1].chainId == network.OP_MAINNET &&
       tokens[2].chainId == network.KOVAN &&
-      tokens[3].chainId == network.OP_KOVAN 
+      tokens[3].chainId == network.OP_KOVAN
     ) {
-      console.log("Success: Token configuration valid. Configuration: Full token suit");
+      console.log(
+        "Success: Token configuration valid. Configuration: Full token suit"
+      );
     } else {
-      throw Error("Tokens provided are not a valid configuration\n" + 
-        "Potential Fix: Please see the README for valid token network configurations."
+      throw Error(
+        "Tokens provided are not a valid configuration\n" +
+          "Potential Fix: Please see the README for valid token network configurations."
       );
     }
-  } else if(tokens.length == 2) {
-    if(
+  } else if (tokens.length == 2) {
+    if (
       tokens[0].chainId == network.OP_MAINNET &&
       tokens[1].chainId == network.OP_KOVAN
     ) {
-      console.log("Success: Token configuration valid. Configuration: Unbridgeable");
-    } else if(
+      console.log(
+        "Success: Token configuration valid. Configuration: Unbridgeable"
+      );
+    } else if (
       tokens[0].chainId == network.KOVAN &&
       tokens[1].chainId == network.OP_KOVAN
     ) {
       console.log("Success: Token configuration valid. Configuration: Testing");
     } else {
-      throw Error("Tokens provided are not a valid configuration\n" + 
-        "Potential Fix: Please see the README for valid token network configurations."
+      throw Error(
+        "Tokens provided are not a valid configuration\n" +
+          "Potential Fix: Please see the README for valid token network configurations."
       );
     }
   } else {
-    throw Error("Tokens provided are not a valid configuration\n" + 
-      "Potential Fix: Please see the README for valid token network configurations."
+    throw Error(
+      "Tokens provided are not a valid configuration\n" +
+        "Potential Fix: Please see the README for valid token network configurations."
     );
   }
+}
+
+function readFile(path: any) {
+  try {
+    fs.readFileSync(path, { encoding: "utf8", flag: "r" });
+  } catch (err) {
+    console.log(err);
+    // TODO throw a real error
+    return "Something went wrong reading the file.";
+  }
+}
+
+function writeFile(path: any, data: any) {
+  try {
+    let stringData = JSON.stringify(data);
+    fs.writeFileSync(path, stringData, {
+      flag: "w"
+    });
+  } catch (err) {
+    console.log(err);
+    // TODO throw a real error
+    return "Something went wrong";
+  }
+}
+
+async function getContractDeployer(
+  address: string,
+  chainId: number
+): Promise<string> {
+  const url = {
+    1: `https://etherscan.io/address/${address}`,
+    10: `https://optimistic.etherscan.io/address/${address}`,
+    42: `https://kovan.etherscan.io/address/${address}`,
+    69: `https://kovan-optimistic.etherscan.io/address/${address}`
+  };
+
+  const response = await fetch(url[chainId]);
+  const body = await response.text();
+  const searchTerm = "Creator Address'>";
+  const index = body.indexOf(searchTerm);
+  const deployerAddress = body.substring(
+    index + searchTerm.length,
+    index + 42 + searchTerm.length
+  );
+  return deployerAddress.includes("GENESIS") ? "GENESIS" : deployerAddress;
 }
 
 main()
