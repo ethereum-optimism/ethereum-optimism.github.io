21d6c2f03da574f8d9a43a71d43e6264a9707a77
Veronica <veronica.21.coutts@gmail.com>
2022-06-29 23:45:46 +0200

    deployer address

--- scripts/validateProjectInfo.ts
+++ scripts/validateProjectInfo.ts
@@ -33,9 +33,28 @@ export const chainIdLayerMap = {
   [network.OP_KOVAN]: 2
 };
 
+enum validConfiguration {
+  testing,
+  fullSuite,
+  unbridgeable
+}
+
+const validConfigurations = {
+  testing: [42, 69],
+  fullSuite: [1, 10, 42, 69],
+  unbridgeable: [10, 69]
+};
+
 const infuraKey = process.env.INFURA_KEY || "84842078b09946638c03157f83405213";
 
 const factoryDeployer:string = "0x4200000000000000000000000000000000000012";
+
+const standardBridgeAddress = {
+  1: "0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1",
+  10: "0x4200000000000000000000000000000000000010",
+  42: "0x22F24361D548e5FaAfb36d1437839f080363982B",
+  69: "0x4200000000000000000000000000000000000010"
+}
 
 const networkURLMap = {
   1: `https://mainnet.infura.io/v3/${infuraKey}`,
@@ -43,6 +62,13 @@ const networkURLMap = {
   42: `https://kovan.infura.io/v3/${infuraKey}`,
   69: `https://kovan.optimism.io`
 };
+
+const extensions = {
+  1: `{"optimismBridgeAddress":"${standardBridgeAddress[1]}"}`,
+  10: `{"optimismBridgeAddress":"${standardBridgeAddress[10]}"}`,
+  42: `{"optimismBridgeAddress":"${standardBridgeAddress[42]}"}`,
+  69: `{"optimismBridgeAddress":"${standardBridgeAddress[69]}"}`
+}
 
 const networkMap = {
   1: "Mainnet",
@@ -76,20 +102,37 @@ async function main() {
 
   // TODO verify off chain data
 
-  // Getting onchain info
-  for (const token of newToken.tokens) {
-    // let onchainListing:TokenListing =
-    await getOnchainInfo(token.chainId, token.address, newToken.logoURI);
-
-    // console.log(token)
-    // console.log(onchainListing)
-
-    // const result = opTokenList.tokens.find( ({ address }) => address === token.address );
-    // if(result != undefined) {
-    //   // TODO check if info is the same in case script has been run before
-    //   throw Error("Token address already listed! Please use update not add.");
-    // }
+  let tokenChains:Array<number> = new Array;
+  for(const token of newToken.tokens) {
+    tokenChains.push(token.chainId);
   }
+  let tokenOrdering:validConfiguration = checkConfig(tokenChains);
+  console.log(tokenOrdering);
+
+  // Getting onchain info
+  let formattedTokens:Array<TokenListing> = new Array;
+  for (const token of newToken.tokens) {
+    let onchainListing: TokenListing = {
+      chainId: token.chainId,
+      address: token.address,
+      name: "",
+      symbol: "",
+      decimals: 0,
+      logoURI: newToken.logoURI,
+      factoryDeployed: false
+    };
+    onchainListing = await getOnchainInfo(onchainListing);
+    onchainListing = await setBridgeAddresses(onchainListing, tokenOrdering);
+
+    formattedTokens.push(onchainListing);
+  }
+  console.log(formattedTokens);
+
+  // const result = opTokenList.tokens.find( ({ address }) => address === token.address );
+  // if(result != undefined) {
+  //   // TODO check if info is the same in case script has been run before
+  //   throw Error("Token address already listed! Please use update not add.");
+  // }
 
   // Checking config is valid
   // A valid config is defined in the README as one of three:
@@ -103,39 +146,60 @@ async function main() {
   // writeFile("./projects/standard-bridge/example.json", newToken);
 }
 
-async function getOnchainInfo(
-  chainId: number,
-  tokenAddress: string,
-  logoURI: string
-): Promise<TokenListing> {
-  let onchainListing: TokenListing = {
-    chainId: chainId,
-    address: tokenAddress,
-    name: "",
-    symbol: "",
-    decimals: 0,
-    logoURI: logoURI,
-    factoryDeployed: false
-  };
+// function formattedFile(
+//   originalFile: any,
 
+// )
+
+async function setBridgeAddresses(
+  onchainListing: TokenListing,
+  tokenConfig: validConfiguration
+): Promise<TokenListing> {
+  if(tokenConfig == validConfiguration.unbridgeable) {
+    return onchainListing;
+  } else if(tokenConfig == validConfiguration.testing) {
+    if(
+      onchainListing.chainId == network.KOVAN || 
+      onchainListing.chainId == network.OP_KOVAN
+    ) {
+      onchainListing.extensions = extensions[onchainListing.chainId];
+    } else {
+      throw Error("Unsupported network");
+    }
+  } else if(tokenConfig == validConfiguration.fullSuite) {
+    if(
+      onchainListing.chainId == network.MAINNET || 
+      onchainListing.chainId == network.OP_MAINNET || 
+      onchainListing.chainId == network.KOVAN || 
+      onchainListing.chainId == network.OP_KOVAN
+    ) {
+      onchainListing.extensions = extensions[onchainListing.chainId];
+    } else {
+      throw Error("Unsupported network");
+    }
+  }
+  return onchainListing;
+}
+
+async function getOnchainInfo(onchainListing: TokenListing): Promise<TokenListing> {
   if (
-    chainId != network.MAINNET &&
-    chainId != network.OP_MAINNET &&
-    chainId != network.KOVAN &&
-    chainId != network.OP_KOVAN
+    onchainListing.chainId != network.MAINNET &&
+    onchainListing.chainId != network.OP_MAINNET &&
+    onchainListing.chainId != network.KOVAN &&
+    onchainListing.chainId != network.OP_KOVAN
   ) {
     throw Error("Chain ID provided is invalid.");
   }
 
   console.log(
-    "Fetch: Info for token " + tokenAddress + " on " + networkMap[chainId]
+    "Fetch: Info for token " + onchainListing.address + " on " + networkMap[onchainListing.chainId]
   );
 
-  const currentNetwork = networkURLMap[chainId];
+  const currentNetwork = networkURLMap[onchainListing.chainId];
   const currentChainProvider = new providers.JsonRpcProvider(currentNetwork);
 
   const contract = new Contract(
-    tokenAddress,
+    onchainListing.address,
     tokenInterface,
     currentChainProvider
   );
@@ -155,21 +219,33 @@ async function getOnchainInfo(
   }
   console.log("Success: Pulled token information.");
 
-  let result = await getContractDeployer(
+  let deployerAddress = await getContractDeployer(
     onchainListing.address,
     onchainListing.chainId
   );
-  console.log(result);
 
-  if(result == factoryDeployer) {
-    
+  if(
+    (onchainListing.chainId == network.OP_MAINNET || 
+    onchainListing.chainId == network.OP_KOVAN) &&
+    deployerAddress == factoryDeployer
+  ) {
+    onchainListing.factoryDeployed = true;
+    console.log("Success: Token was deployed by factory on L2."); 
+  } else if(
+    onchainListing.chainId == network.MAINNET || 
+    onchainListing.chainId == network.KOVAN
+  ) { 
+    onchainListing.factoryDeployed = true;
+    console.log("Deployed on an L1");
+  } else {
+    throw Error("Token not deployed by ERC20 factory.\n" + 
+      "Potential Fix: Please use the appropriate token adder as specified in the base README."
+    );
   }
-
-  console.log(onchainListing);
   return onchainListing;
 }
 
-function checkConfig(tokens: Array<TokenListing>) {
+function checkConfig(tokens: Array<number>): validConfiguration {
   if (tokens.length > 4) {
     throw Error(
       "Max tokens provided is greater than 4.\n" +
@@ -179,14 +255,15 @@ function checkConfig(tokens: Array<TokenListing>) {
     );
   } else if (tokens.length == 4) {
     if (
-      tokens[0].chainId == network.MAINNET &&
-      tokens[1].chainId == network.OP_MAINNET &&
-      tokens[2].chainId == network.KOVAN &&
-      tokens[3].chainId == network.OP_KOVAN
+      tokens[0] == network.MAINNET &&
+      tokens[1] == network.OP_MAINNET &&
+      tokens[2] == network.KOVAN &&
+      tokens[3] == network.OP_KOVAN
     ) {
       console.log(
         "Success: Token configuration valid. Configuration: Full token suit"
       );
+      return validConfiguration.fullSuite;
     } else {
       throw Error(
         "Tokens provided are not a valid configuration\n" +
@@ -195,17 +272,19 @@ function checkConfig(tokens: Array<TokenListing>) {
     }
   } else if (tokens.length == 2) {
     if (
-      tokens[0].chainId == network.OP_MAINNET &&
-      tokens[1].chainId == network.OP_KOVAN
+      tokens[0] == network.OP_MAINNET &&
+      tokens[1] == network.OP_KOVAN
     ) {
       console.log(
         "Success: Token configuration valid. Configuration: Unbridgeable"
       );
+      return validConfiguration.unbridgeable;
     } else if (
-      tokens[0].chainId == network.KOVAN &&
-      tokens[1].chainId == network.OP_KOVAN
+      tokens[0] == network.KOVAN &&
+      tokens[1] == network.OP_KOVAN
     ) {
       console.log("Success: Token configuration valid. Configuration: Testing");
+      return validConfiguration.testing;
     } else {
       throw Error(
         "Tokens provided are not a valid configuration\n" +
