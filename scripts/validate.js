f0d1f1d13b34dd7740499363cd46130ee392f07e
Kelvin Fichter <kelvin@optimism.io>
2022-08-19 13:41:17 -0400

    feat(ci): only check changed files
    Limits validation to only changed files, speeding up CI time and setting
    us up for a few upcoming PRs.

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,14 +1,22 @@
-const { generate } = require('../src/generate')
+const { generate } = require("../src/generate");
 
 const main = async () => {
-  await generate()
+  await generate(
+    process.argv.slice(2)
+      .filter((file) => {
+        return file.startsWith('data')
+      })
+      .map((file) => {
+        return file.split('/')[1]
+      })
+  );
 }
 
 main()
   .catch((err) => {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   })
   .finally(() => {
-    process.exit(0)
+    process.exit(0);
   })
