a8505960a38b62a3254ddbe89545c99b63134f69
Alex Harley <alex@alexbh.dev>
2024-01-25 19:31:29 +1300

    feat: add base-sepolia config

--- src/types.ts
+++ src/types.ts
@@ -27,11 +27,12 @@ export type Chain =
   | 'optimism-goerli'
   | 'optimism-sepolia'
   | 'base-goerli'
+  | 'base-sepolia'
   | 'pgn'
   | 'pgn-sepolia'
   | 'sepolia'
 
-const l2Chains = ['optimism', 'optimism-goerli', 'optimism-sepolia', 'base', 'base-goerli', 'pgn-sepolia', 'pgn'] as const
+const l2Chains = ['optimism', 'optimism-goerli', 'optimism-sepolia', 'base', 'base-goerli', 'base-sepolia', 'pgn-sepolia', 'pgn'] as const
 export type L2Chain = typeof l2Chains[number]
 
 export const isL2Chain = (chain: string): chain is L2Chain => {
