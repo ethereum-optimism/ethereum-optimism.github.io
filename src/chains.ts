528da5f2ade4cf64ff2e107b2e4de618ecd06b1b
iain nash <me@iain.in>
2024-02-26 17:06:08 -0500

    Add ZORA network details

--- src/chains.ts
+++ src/chains.ts
@@ -23,7 +23,7 @@ export const NETWORK_DATA: Record<Chain, Network> = {
     id: 8453,
     name: 'Base',
     provider: new ethers.providers.StaticJsonRpcProvider(
-      'https://mainnet.base.org',
+      'https://mainnet.base.org'
     ),
     layer: 2,
   },
@@ -39,7 +39,7 @@ export const NETWORK_DATA: Record<Chain, Network> = {
     id: 34443,
     name: 'Mode',
     provider: new ethers.providers.StaticJsonRpcProvider(
-      'https://mainnet.mode.network',
+      'https://mainnet.mode.network'
     ),
     layer: 2,
   },
@@ -52,7 +52,10 @@ export const NETWORK_DATA: Record<Chain, Network> = {
   sepolia: {
     id: 11155111,
     name: 'Sepolia',
-    provider: new ethers.providers.StaticJsonRpcProvider(`https://sepolia.infura.io/v3/${DEFAULT_INFURA_KEY}`, 11155111),
+    provider: new ethers.providers.StaticJsonRpcProvider(
+      `https://sepolia.infura.io/v3/${DEFAULT_INFURA_KEY}`,
+      11155111
+    ),
     layer: 1,
   },
   'optimism-goerli': {
@@ -97,6 +100,22 @@ export const NETWORK_DATA: Record<Chain, Network> = {
     ),
     layer: 2,
   },
+  zora: {
+    id: 7777777,
+    name: 'Zora',
+    provider: new ethers.providers.StaticJsonRpcProvider(
+      'https://rpc.zora.energy',
+      7777777
+    ),
+  },
+  'zora-sepolia': {
+    id: 99999999,
+    name: 'Zora Sepolia',
+    provider: new ethers.providers.StaticJsonRpcProvider(
+      'https://sepolia.rpc.zora.energy',
+      99999999
+    ),
+  },
 }
 
 interface L2BridgeInformation {
@@ -139,6 +158,12 @@ export const L2_STANDARD_BRIDGE_INFORMATION: Record<
   'pgn-sepolia': {
     l2StandardBridgeAddress: '0x4200000000000000000000000000000000000010',
   },
+  zora: {
+    l2StandardBridgeAddress: '0x4200000000000000000000000000000000000010',
+  },
+  'zora-sepolia': {
+    l2StandardBridgeAddress: '0x4200000000000000000000000000000000000010',
+  },
 }
 
 export const L2_TO_L1_PAIR: Partial<Record<L2Chain, L1Chain>> = {
@@ -150,7 +175,7 @@ export const L2_TO_L1_PAIR: Partial<Record<L2Chain, L1Chain>> = {
   'optimism-sepolia': 'sepolia',
   'base-goerli': 'goerli',
   'base-sepolia': 'sepolia',
-  'pgn-sepolia': 'sepolia'
+  'pgn-sepolia': 'sepolia',
 }
 
 export const L1_STANDARD_BRIDGE_INFORMATION: Record<
@@ -173,6 +198,10 @@ export const L1_STANDARD_BRIDGE_INFORMATION: Record<
     {
       l2Chain: 'mode',
       l1StandardBridgeAddress: '0x735aDBbE72226BD52e818E7181953f42E3b0FF21',
+    },
+    {
+      l2Chain: 'zora',
+      l1StandardBridgeAddress: '0x3e2Ea9B92B7E48A52296fD261dc26fd995284631',
     },
   ],
   goerli: [
@@ -198,5 +227,9 @@ export const L1_STANDARD_BRIDGE_INFORMATION: Record<
       l2Chain: 'base-sepolia',
       l1StandardBridgeAddress: '0xfd0Bf71F60660E2f608ed56e1659C450eB113120',
     },
-  ]
+    {
+      l2Chain: 'zora-sepolia',
+      l1StandardBridgeAddress: '0x5376f1D543dcbB5BD416c56C189e4cB7399fCcCB',
+    },
+  ],
 }
