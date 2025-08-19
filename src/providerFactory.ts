d1cd165efde4624f2e10991db4d096780080bce9
fainashalts <fainashalts@gmail.com>
2025-08-19 13:08:06 -0700

    replace Infura with Alchemy

--- src/providerFactory.ts
+++ src/providerFactory.ts
@@ -62,78 +62,78 @@ export const getRpcUrls = (chain: string): string[] => {
  * Get Alchemy API key from environment variable
  */
 const getAlchemyApiKey = (): string => {
-  return process.env.ALCHEMY_API_KEY || ''
+    return process.env.ALCHEMY_API_KEY || ''
 }
 
 /**
  * Default RPC URLs with Alchemy endpoints and fallbacks
  */
 const getDefaultRpcUrls = (chain: string): string[] => {
-  const alchemyApiKey = getAlchemyApiKey()
-  
-  const rpcMap: Record<string, string[]> = {
-    ethereum: [
-      ...(alchemyApiKey ? [`https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://eth.llamarpc.com',
-      'https://rpc.ankr.com/eth',
-      'https://ethereum.publicnode.com'
-    ],
-    sepolia: [
-      ...(alchemyApiKey ? [`https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://rpc.sepolia.org',
-      'https://ethereum-sepolia.publicnode.com'
-    ],
-    optimism: [
-      ...(alchemyApiKey ? [`https://opt-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://mainnet.optimism.io',
-      'https://optimism.llamarpc.com',
-      'https://optimism.publicnode.com'
-    ],
-    'optimism-sepolia': [
-      ...(alchemyApiKey ? [`https://opt-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://sepolia.optimism.io'
-    ],
-    base: [
-      ...(alchemyApiKey ? [`https://base-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://mainnet.base.org',
-      'https://base.llamarpc.com',
-      'https://base.publicnode.com'
-    ],
-    'base-sepolia': [
-      ...(alchemyApiKey ? [`https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://sepolia.base.org'
-    ],
-    // Alchemy supports more chains than Infura
-    polygon: [
-      ...(alchemyApiKey ? [`https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://polygon-rpc.com'
-    ],
-    arbitrum: [
-      ...(alchemyApiKey ? [`https://arb-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
-      'https://arb1.arbitrum.io/rpc'
-    ],
-    // Chains not supported by Alchemy - use native endpoints
-    unichain: [
-      'https://mainnet.unichain.org'
-    ],
-    'unichain-sepolia': [
-      'https://sepolia.unichain.org'
-    ],
-    mode: ['https://mainnet.mode.network'],
-    lisk: ['https://rpc.api.lisk.com'],
-    'lisk-sepolia': ['https://rpc.sepolia-api.lisk.com'],
-    redstone: ['https://rpc.redstonechain.com'],
-    metall2: ['https://rpc.metall2.com'],
-    'metall2-sepolia': ['https://testnet.rpc.metall2.com'],
-    soneium: ['https://rpc.soneium.org'],
-    'soneium-minato': ['https://rpc.minato.soneium.org'],
-    celo: ['https://forno.celo.org'],
-    swellchain: ['https://swell-mainnet.alt.technology'],
-    ink: ['https://rpc-gel.inkonchain.com'],
-    'ink-sepolia': ['https://rpc-gel-sepolia.inkonchain.com'],
-    worldchain: ['https://worldchain-mainnet.g.alchemy.com/public'],
-    'worldchain-sepolia': ['https://worldchain-sepolia.g.alchemy.com/public'],
-  }
+    const alchemyApiKey = getAlchemyApiKey()
 
-  return rpcMap[chain] || []
+    const rpcMap: Record<string, string[]> = {
+        ethereum: [
+            ...(alchemyApiKey ? [`https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://eth.llamarpc.com',
+            'https://rpc.ankr.com/eth',
+            'https://ethereum.publicnode.com'
+        ],
+        sepolia: [
+            ...(alchemyApiKey ? [`https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://rpc.sepolia.org',
+            'https://ethereum-sepolia.publicnode.com'
+        ],
+        optimism: [
+            ...(alchemyApiKey ? [`https://opt-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://mainnet.optimism.io',
+            'https://optimism.llamarpc.com',
+            'https://optimism.publicnode.com'
+        ],
+        'optimism-sepolia': [
+            ...(alchemyApiKey ? [`https://opt-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://sepolia.optimism.io'
+        ],
+        base: [
+            ...(alchemyApiKey ? [`https://base-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://mainnet.base.org',
+            'https://base.llamarpc.com',
+            'https://base.publicnode.com'
+        ],
+        'base-sepolia': [
+            ...(alchemyApiKey ? [`https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://sepolia.base.org'
+        ],
+        // Alchemy supports more chains than Infura
+        polygon: [
+            ...(alchemyApiKey ? [`https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://polygon-rpc.com'
+        ],
+        arbitrum: [
+            ...(alchemyApiKey ? [`https://arb-mainnet.g.alchemy.com/v2/${alchemyApiKey}`] : []),
+            'https://arb1.arbitrum.io/rpc'
+        ],
+        // Chains not supported by Alchemy - use native endpoints
+        unichain: [
+            'https://mainnet.unichain.org'
+        ],
+        'unichain-sepolia': [
+            'https://sepolia.unichain.org'
+        ],
+        mode: ['https://mainnet.mode.network'],
+        lisk: ['https://rpc.api.lisk.com'],
+        'lisk-sepolia': ['https://rpc.sepolia-api.lisk.com'],
+        redstone: ['https://rpc.redstonechain.com'],
+        metall2: ['https://rpc.metall2.com'],
+        'metall2-sepolia': ['https://testnet.rpc.metall2.com'],
+        soneium: ['https://rpc.soneium.org'],
+        'soneium-minato': ['https://rpc.minato.soneium.org'],
+        celo: ['https://forno.celo.org'],
+        swellchain: ['https://swell-mainnet.alt.technology'],
+        ink: ['https://rpc-gel.inkonchain.com'],
+        'ink-sepolia': ['https://rpc-gel-sepolia.inkonchain.com'],
+        worldchain: ['https://worldchain-mainnet.g.alchemy.com/public'],
+        'worldchain-sepolia': ['https://worldchain-sepolia.g.alchemy.com/public'],
+    }
+
+    return rpcMap[chain] || []
 }
